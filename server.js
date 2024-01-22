a2a91ff92c3486ba65a8fbb1e6db8f90004df0fb
javedbartlett <javedbartlett@gmail.com>
2024-01-22 05:13:35 +0000

    server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const PORT = 3001;
+
+const server = http.createServer(function(req, res) {
+	res.writeHead(200);
+	res.write("You just got Litt up!");
+	res.end();
+});
+
+server.listen(PORT, () => console.log(`Javed's server started on port ${PORT}`));
